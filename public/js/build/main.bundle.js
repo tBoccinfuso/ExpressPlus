f319d7fb7d8213ae931925c3a51ba9776ddb0ac7
Thomas Boccinfuso <tboccinfuso@gmail.com>
2018-05-12 23:57:51 -0500

    fixed folder structure

--- public/js/build/main.bundle.js
+++ public/js/build/main.bundle.js
@@ -8,4 +8,21 @@ var childNode = document.createElement('p');
 childNode.innerHTML = helloWorld;
 
 // Select out empty div and append!
-document.getElementById('es6Demo').appendChild(childNode);
+if (document.getElementById('es6Demo')) document.getElementById('es6Demo').appendChild(childNode);
+
+//Set button onClick events to navigate the site
+if (document.getElementById('btnLinks')) document.getElementById('btnLinks').addEventListener('click', function () {
+  location.href = location.href + "links";
+});
+
+// Navigate to /hello 
+if (document.getElementById('btnHello')) document.getElementById('btnHello').addEventListener('click', function () {
+  location.href = location.href + "hello";
+});
+
+// Navigate to /hello 
+if (document.getElementById('btnHome')) document.getElementById('btnHome').addEventListener('click', function () {
+  var loc = location.href.lastIndexOf('/');
+  var x = location.href.slice(0, loc);
+  location.href = x;
+});
